6c764f321a669b63fdf845015c9073aee7977479
BecaSSND <98946394+BecaSSND@users.noreply.github.com>
2025-10-21 18:03:48 +0200

    added WebAPI

---
+++ WebApi/Controllers/UsersController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetAllUsers()
+    {
+        return Ok("Users endpoint works!");
+    }
+}
